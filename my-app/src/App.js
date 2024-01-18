8ac1bd2d805078305417062efc11e015bd4c0f51
김동욱 <ehddnr73@naver.com>
2024-01-18 15:28:36 +0900

    props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) { // props(Object type)를 받아서 표현식을 사용하여 동적으로 설정
   return (
     <header>
-        <h1><a href="/">WEB</a></h1>
+        <h1><a href="/">{props.title}</a></h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+
+  props.topics.forEach((topic) => {
+    lis.push(<li key={topic.id}><a href={'/read/' + topic.id}>{topic.title}</a></li>);
+  });
+
   return (
     <nav>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
+          {lis}
         </ol>
     </nav>
   );
 }
 
-function Article() {
+function Article(props) {
   return (
     <article>
-        <h2>Welcome</h2>
-        Hello, WEB
+        <h2>{props.title}</h2>
+        {props.body}
     </article>
-  )
+  );
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body: 'html is ...'}, 
+    {id:2, title:'css', body: 'css is ...'}, 
+    {id:3, title:'javascript', body: 'javascript is ...'}
+  ];
+
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="Hello"></Header>
+      <Header title="World"></Header>
+      {/* 객체 또는 Number를 전달하기 위해서 표현식을 사용 */}
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, world!!"></Article>
+      <Article title={1}></Article>
     </div>
   );
 }
