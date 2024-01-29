8bb4077b6d3e0be718be0c12f0480d6106b924a5
김동욱 <ehddnr73@naver.com>
2024-01-29 13:36:46 +0900

    create

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -45,6 +45,23 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event => {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"/></p>
+    </form>
+  </article>
+}
+
 function App() {
   // const _mode = useState('WELCOME');
   // const mode = _mode[0];
@@ -52,12 +69,13 @@ function App() {
 
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
-  const topics = [
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body: 'html is ...'}, 
     {id:2, title:'css', body: 'css is ...'}, 
     {id:3, title:'javascript', body: 'javascript is ...'}
-  ];
+  ]);
 
   let content = null;
 
@@ -73,6 +91,28 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body) => {
+      /*
+      const [value, setValue] = useState(PRIMITIVE);
+      ex) string, number, bigint, boolean, undefined, symbol, null
+
+      const [value, setValue] = useState(Object);
+      ex) object, array
+      객체일 경우의 복제 시퀀스
+      1. newValue = {...value}; // 복제
+      2. newValue 변경
+      3. setValue(newValue);
+      */
+      const newTopic = { id: nextId, title: _title,  body: _body };
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
   }
 
   return (
@@ -86,7 +126,11 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      <Article title={1}></Article>
+      <a href="/create" onClick={event => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
+      {/* <Article title={1}></Article> */}
     </div>
   );
 }
